9a4b27d4fa4c9ce55780d88f305236a070ff7de5
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-11-11 17:17:53 -0300

    Finalizando a primeira parte do projeto (Html, Css e Javascript)

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,38 @@
-// // Detecta o evento de rolagem
-// window.addEventListener("scroll", function() {
-//     const image = document.querySelector(".image");
+const API_KEY = `live_bewFyXoIDW5hEUkiaPjfNJ4zuq5ViN4C2zGSKRpaeWhgasQ7BQHdDnvcavZggXPI`;
+const URL = `https://api.thecatapi.com/v1/images/search?limit=9&api_key=${API_KEY}`;
 
-//     // Verifica se o usuário rolou para baixo
-//     if (window.scrollY > 50) { // Ajuste o valor conforme necessário
-//         image.classList.add("scrolled"); // Aplica o gradiente
-//     } else {
-//         image.classList.remove("scrolled"); // Remove o gradiente
-//     }
-// });
+// Busca as imagens na API
+const searchData = async () => {
+    const res = document.getElementById('res');
+    
+    try {
+        const response = await fetch(URL);
+        if (!response.ok) {
+            res.innerHTML = 'Erro ao buscar imagens!';
+        } else {
+            const object = await response.json();
+            res.innerHTML = ''; // Limpa o contêiner antes de adicionar novas imagens
+            
+            object.forEach((e) => {
+                const img = document.createElement('img');
+                img.src = e.url;
+                img.alt = 'Cats Images';
+                res.appendChild(img); // Adiciona a imagem ao contêiner
+            });
+        }
+    } catch (error) {
+        res.innerHTML = 'ERROR!';
+    }
+};
 
+// Função para tocar o áudio e buscar as imagens
+function FetchData() {
+    let audio = new Audio('../assets/meow.m4a');
+    audio.play(); // Toca o áudio
 
+    // Torna o contêiner de imagens visível
+    const res = document.getElementById('res');
+    res.style.display = 'grid'; // Exibe o contêiner de imagens como grid
 
-const 
+    searchData(); // Chama a função que busca as imagens da API
+}

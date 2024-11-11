8a6b8fa95cdb64fbe7660c7a02791c60a03eb8cd
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-11-11 16:38:15 -0300

    html e css - parte 1

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+// // Detecta o evento de rolagem
+// window.addEventListener("scroll", function() {
+//     const image = document.querySelector(".image");
+
+//     // Verifica se o usuário rolou para baixo
+//     if (window.scrollY > 50) { // Ajuste o valor conforme necessário
+//         image.classList.add("scrolled"); // Aplica o gradiente
+//     } else {
+//         image.classList.remove("scrolled"); // Remove o gradiente
+//     }
+// });
+
+
+
+const 
